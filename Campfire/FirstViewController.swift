6ba290550268535134ef88f7dcab795c1eecb76b
Lauren Nicole Roth <laurennicoleroth@gmail.com>
2015-05-06 20:37:53 -0400

    Initial Commit

---
+++ Campfire/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Campfire
+//
+//  Created by Lauren Roth on 5/6/15.
+//  Copyright (c) 2015 Lauren Roth. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
