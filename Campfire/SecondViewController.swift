bdfe850aa544131be011ae522d875b4e1d74b9a3
Lauren Nicole Roth <laurennicoleroth@gmail.com>
2015-05-07 15:46:59 -0400

    Add button, label, textfield ui to firstview.

--- Campfire/SecondViewController.swift
+++ Campfire/SecondViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +19,6 @@ class SecondViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
